d6be2ab25ff53008f9c3bf55ef66fc5f7479c486
Jerathel Jean <jeratheljean@yahoo.com>
2022-09-12 17:57:26 -0400

    Initial commit

---
+++ lib/splashScreen/splash_screen.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MySplashScreen> createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+
+  startTimer()
+  {
+    Timer(Duration(seconds: 3), () async {
+      //send user to home screen
+      Navigator.push(context, MaterialPageRoute(builder: builder))
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
